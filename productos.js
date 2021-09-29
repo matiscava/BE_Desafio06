994d1bf7ab79a2f4e67dbe86dca967cd073376eb
matiscava <83254612+matiscava@users.noreply.github.com>
2021-09-29 20:48:26 -0300

    Desafio clase 6

---
+++ productos.js
@@ -0,0 +1,74 @@
+module.exports = class ArchivoFS { 
+    constructor (archivo) {
+        this.archivo=archivo;
+    }
+    getAll(){
+        const fs = require('fs');
+        const data = fs.readFileSync(this.archivo);
+        const objeto = JSON.parse(data);
+        return objeto
+    }
+    save(titulo,precio,url,archivo){
+        const fs = require('fs');
+        const data = fs.readFileSync(this.archivo);
+        const objeto = JSON.parse(data);
+        let nextID = 1
+        let agregarData;
+        if(objeto.length===0){
+            agregarData = {
+                "id": nextID,
+                "title": titulo,
+                "price": precio,
+                "thumbnail":url
+            }
+        }else{
+            for (let i=0;i<objeto.length ;i++) {
+                while( objeto[i].id >= nextID ){
+                    nextID++;
+                }
+                agregarData = {
+                    "id": nextID,
+                    "title": titulo,
+                    "price": precio,
+                    "thumbnail":url
+                }
+            }
+        }
+        console.log('agregarData',agregarData)
+        objeto.push(agregarData);
+        const dataToJSON = JSON.stringify(objeto,null,2);
+        fs.writeFileSync(archivo, dataToJSON);
+        console.log('data:',objeto);
+        }
+    deleteById (idNum) {
+        const fs = require('fs');
+        const data = fs.readFileSync(this.archivo);
+        const objeto = JSON.parse(data);
+        const objetoFiltrado = objeto.filter(obj => obj.id !== idNum);
+        console.log('objeto filtrado:',objetoFiltrado);
+        const dataToJSON = JSON.stringify(objetoFiltrado,null,2);
+        fs.writeFileSync('./productList.json', dataToJSON);
+    }
+    getById (idNum) {
+        const fs = require('fs');
+        const data = fs.readFileSync(this.archivo);
+        const objeto = JSON.parse(data);
+        const objetoFiltrado = objeto.filter(obj => obj.id === idNum);
+        if (objetoFiltrado[0]===undefined) {
+         console.log('objeto filtrado:',null);
+         return null
+        }else{
+            console.log('objeto filtrado:',objetoFiltrado[0]);
+        } 
+        return objetoFiltrado[0]       
+    }
+    
+    deleteAll () {
+        const fs = require('fs');
+        const data = fs.readFileSync(this.archivo);
+        // const objeto = JSON.parse(data);
+        const obejtoVacio = [];
+        const dataToJSON = JSON.stringify(obejtoVacio,null,2);
+        fs.writeFileSync('./productList.json', dataToJSON);
+    }
+}
---
+++ server.js
@@ -0,0 +1,67 @@
+const express = require('express');
+const fs = require('fs');
+const server = express();
+const ArchivoFS = require('./productos');
+
+const PORT = 8080
+async function cargarTxt(){
+    try {
+        const productos = await new ArchivoFS('./producto.txt')
+        // console.log(productos)
+        productos.save('Iphone 11',45200,'https://www.casanissei.com/media/catalog/product/cache/16a9529cefd63504739dab4fc3414065/1/1/11_negrooo_9.jpg','./producto.txt')
+        productos.save('Game Boy Color',11231,'https://upload.wikimedia.org/wikipedia/commons/thumb/7/76/Nintendo-Game-Boy-Color-FL.jpg/300px-Nintendo-Game-Boy-Color-FL.jpg','./producto.txt')
+        productos.save('Gallina',2015436,'https://pbs.twimg.com/media/EKaEW7AW4AEHg-q?format=jpg&name=small','./producto.txt')
+        leerJson()
+    }
+    catch(error){
+        console.log('error de lectura',error);
+
+    }
+}
+async function leerJson () {
+    try{
+        const contenido = await new ArchivoFS('./productList.json')
+            // console.log(contenido.length());
+            const listado = contenido.getAll();
+
+            server.get('/', (solicitud, respuesta, siguiente)=> {
+                respuesta.send('<h1 style="color:blue">Bienvenido al Desafio de la Clase 6</h1>')
+            });
+            server.get('/productos', (solicitud, respuesta, siguiente)=> {
+                respuesta.send(contenido.getAll())
+            });
+            server.get('/productoRandom', (solicitud, respuesta, siguiente)=> {
+                const maximo = listado.length;
+                console.log('maximo: ',maximo)
+                idRandom = Math.floor(Math.random()*(maximo))+1;
+
+
+                respuesta.send(contenido.getById(idRandom))
+            });
+
+            server.listen(PORT,()=>{
+                console.log(`Servidor corriendo en el puerto: ${PORT}`);
+            })
+
+            server.on('error', (error) => console.log(`Error en servidor ${error}`));
+    }
+    catch(error){
+        console.log('error de lectura',error);
+    }
+}
+
+cargarTxt()
+
+
+// server.get('/visitas', (solicitud, respuesta, siguiente)=> {
+    
+// });
+
+// server.get('/fyh', (solicitud, respuesta, siguiente)=> {
+//     const date = new Date().toLocaleString();
+//     respuesta.json({fyh: date})
+//     // respuesta.send({fyh: today.format('L LTS')})
+
+// });
+
+
